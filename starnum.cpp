ca26a9764a4af9110d3e06c8cf8adc847f2136bf
yasamankouchaki <166714649+yasamankouchaki@users.noreply.github.com>
2024-06-04 15:59:09 -0700

    Add files via upload

---
+++ starnum.cpp
@@ -0,0 +1,13 @@
+//print star based on input number.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number:\n";
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        cout<<'*';
+    }
+    return 0;
+}
